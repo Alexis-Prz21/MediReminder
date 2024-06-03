1ecbdd5ab25fc94e12b1bfd328e12a11cd3da741
Alexis-Prz21 <mp21030@ues.edu.sv>
2024-06-03 00:13:57 -0600

    Cambios en vistas y archivos principales de aplicacion

--- app/src/main/java/com/example/proyectopdm2024_gt2_grupo1_tema/HistorialMedicacionActivity.kt
+++ app/src/main/java/com/example/proyectopdm2024_gt2_grupo1_tema/HistorialMedicacionActivity.kt
@@ -2,10 +2,20 @@ package com.example.proyectopdm2024_gt2_grupo1_tema
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HistorialMedicacionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_historial_medicacion)
+
+        /* Inicializando el bottom navigation view
+           Creando el objeto bottom navigation view
+         */
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
